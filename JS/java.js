ff7d4b5dfa77b48e1d5dd6e6f7b3a78f3236b8fa
Md Ariful Islam <2215151056@uits.edu.bd>
2024-09-25 22:08:32 +0600

    first commit

---
+++ JS/java.js
@@ -0,0 +1,15 @@
+// console.log('Attached')
+document.getElementById('login-btn').addEventListener('click', function (event) {
+    event.preventDefault();
+    const phoneNumber = document.getElementById('phone-number').value;
+    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(pinNumber)
+
+    if(phoneNumber==='01604' && pinNumber==='9285'){
+        console.log('Logging In');
+    }
+    else{
+        alert('You have inputed wrong Phone or Pin');
+    }
+});
